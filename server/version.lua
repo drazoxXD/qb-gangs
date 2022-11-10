16b5eb51d03fcd0a5ec8659e6804b9e46339c25d
Drazox <68930610+drazoxXD@users.noreply.github.com>
2022-11-10 12:55:36 +0100

    Fixed version check
    Bcuz this thing was fucked

--- server/version.lua
+++ server/version.lua
@@ -4,7 +4,7 @@ Citizen.CreateThread(
         if vRaw then
             local v = json.decode(vRaw)
             PerformHttpRequest(
-                "https://raw.githubusercontent.com/Mojito-Fivem/qb-gangs/main/version.json",
+                "https://raw.githubusercontent.com/Dhruvpamnani/qb-gangs-create-gang-in-game/main/version.json",
                 function(code, res, headers)
                     if code == 200 then
                         local rv = json.decode(res)
